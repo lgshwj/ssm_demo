5c2826f294d50ef404966a3fe1f11ffbbb8478e7
angluaJs <1220063463@qq.com>
2019-04-19 20:21:14 +0800

    spring mvc 测试

---
+++ src/main/java/org/demo/controller/MyController.java
@@ -0,0 +1,18 @@
+package org.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("myController")
+@RequestMapping("/my")
+public class MyController {
+
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+}
